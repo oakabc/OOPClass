effac33521eaa70e197dfc486c4b3b5fa0a4e936
Oäk Kingkaew <oakabc@gmail.com>
2024-07-13 09:57:36 +0700

    Update Math24.java
    Make the code more clean

--- Math24.java
+++ Math24.java
@@ -3,13 +3,12 @@ public class Math24 {
     public static void main(String [] args){ // Main method
 
         // Declare the variables
-        int A = 5, B = 11, C = 99, D = 29, E = 8, F = 1, H = 3;
-        double G = 5.5;
+        double A = 5, B = 11, C = 99, D = 29, E = 8, F = 1, G = 5.5, H = 3;
 
-        // Calculation    B * C / (D * E) = 4.6939..
+        // Calculation   
         double result;
 
-        result = A + 1.0 * B * C / (D * E) - (F + G) - H;
+        result = A + B * C / (D * E) - (F + G) - H;
 
         // Output
 
