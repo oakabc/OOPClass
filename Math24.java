17a7b7018ea87da38c230025ccb9ff3949c7cf3c
Oäk Kingkaew <oakabc@gmail.com>
2024-07-13 09:55:13 +0700

    Add files via upload

---
+++ Math24.java
@@ -0,0 +1,19 @@
+public class Math24 {
+
+    public static void main(String [] args){ // Main method
+
+        // Declare the variables
+        int A = 5, B = 11, C = 99, D = 29, E = 8, F = 1, H = 3;
+        double G = 5.5;
+
+        // Calculation    B * C / (D * E) = 4.6939..
+        double result;
+
+        result = A + 1.0 * B * C / (D * E) - (F + G) - H;
+
+        // Output
+
+        System.out.println(result);
+
+    }
+}
